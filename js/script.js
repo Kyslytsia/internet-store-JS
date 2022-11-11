aaa7a046a3ceb18e5c39f5a49f0116a5e864e83a
Denys <kislica779@gmail.com>
2022-11-11 18:23:03 +0200

    Store

--- js/script.js
+++ js/script.js
@@ -1,27 +1,29 @@
 import { PRODUCTS, DB } from "./data.js";
 
-const USERS = [
-  ...DB,
-  {
-    name: "Ivan",
-    email: "ivan@gmail.com",
-    password: "123",
-    favourites: [9, 18, 7],
-    orders: [
-      { id: 5, count: 2 },
-      { id: 6, count: 1 },
-    ],
-    status: false,
-  },
-];
+const USERS = DB;
+let userLogined = USERS.find((user) => Boolean(user.status));
 
-const userLogined = USERS.find((user) => Boolean(user.status));
+const testIvan = {
+  name: "Ivan",
+  email: "testivan@gmail.com",
+  password: "123",
+  favourites: [9, 18, 7],
+  orders: [
+    { id: 5, count: 2 },
+    { id: 6, count: 1 },
+  ],
+  status: false,
+};
 
-// Registartion FORM
+if (USERS.findIndex((user) => user.email === testIvan.email) < 0) {
+  USERS.push(testIvan);
+  localStorage.setItem("users", JSON.stringify(USERS));
+}
+
+// REGISTRATION FORM
 // =================
 
 const registrationForm = document.querySelector("#RegistrationForm");
-
 if (registrationForm) {
   registrationForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -74,9 +76,8 @@ if (registrationForm) {
   });
 }
 
-// Login FORM
+// LOGIN FORM
 // ==========
-
 const loginForm = document.querySelector(`#LoginForm`);
 
 if (loginForm) {
@@ -108,6 +109,7 @@ if (loginForm) {
     }
 
     user.status = true;
+
     localStorage.setItem("users", JSON.stringify(USERS));
 
     document.location.href = "account.html";
@@ -126,14 +128,14 @@ document.addEventListener("DOMContentLoaded", (e) => {
   );
   const headerLogout = document.querySelector(`#headerLogout`);
 
-  const userLogined = USERS.find((user) => Boolean(user.status));
-
   if (userLogined) {
     headerUser.href = `account.html`;
     headerUser.innerText = userLogined.name;
 
     headerFavourites.href = `favourites.html`;
     headerFavouritesCount.innerText = userLogined.favourites?.length ?? 0;
+
+    userInfoEmail && (userInfoEmail.innerText = userLogined.email);
 
     headerLogout.classList.add(`active`);
     headerLogout.addEventListener("click", (e) => {
@@ -144,3 +146,246 @@ document.addEventListener("DOMContentLoaded", (e) => {
     });
   }
 });
+
+// ACCOUNT
+// =======
+
+const orderTable = document.querySelector(`#orderTable`);
+if (orderTable && userLogined) {
+  userLogined.orders.forEach((order) => {
+    let product = PRODUCTS.find((product) => order.id === product.id);
+
+    const tr = document.createElement("tr");
+    tr.innerHTML = `<tr>
+                      <td>
+                        <div class="item__info"><img src="images/products/${
+                          product.img
+                        }.png" 
+                        alt="${product.title}" 
+                        height="100">
+                            <div>
+                            <p class="item__info--title">${product.title}</p>
+                            </div>
+                        </div>
+                      </td>
+                      <td>$${product.price}</td>
+                      <td><span class="item__sale">- ${
+                        product.sale ? product.salePercent + "%" : ""
+                      }</span></td>
+                      <td>${order.count}</td>
+                      <td>$${
+                        (product.price -
+                          (product.sale
+                            ? (product.price * product.salePercent) / 100
+                            : 0)) *
+                        order.count
+                      }
+                      </td>
+                    </tr>`;
+    orderTable.append(tr);
+  });
+}
+
+// ACCOUNT BUTTONS
+// ===============
+
+const userInfo = document.querySelector("#userInfo");
+if (userInfo) {
+  userInfo.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const newName = e.target.querySelector("input").value;
+    userLogined.name = newName;
+    localStorage.setItem("users", JSON.stringify(USERS));
+    window.location.reload();
+  });
+}
+
+const deleteAcc = document.querySelector("#deleteAcc");
+if (deleteAcc) {
+  deleteAcc.addEventListener("click", () => {
+    if (userLogined) {
+      const index = USERS.findIndex((user) => Boolean(user.status));
+      USERS.splice(index, 1);
+      localStorage.setItem("users", JSON.stringify(USERS));
+    }
+    document.location.href = "login.html";
+  });
+}
+
+// FAVOURITES
+// ==========
+
+const favouriteTable = document.querySelector("#favouriteTable")?.children[2];
+
+function renderFavourite(user) {
+  user.favourites.forEach((favouriteID) => {
+    const favouriteProduct = PRODUCTS.find(
+      (product) => product.id === favouriteID
+    );
+
+    const tr = document.createElement("tr");
+    tr.innerHTML = `<tr><td>
+                          <div class="item__info">
+                              <img src="images/products/${
+                                favouriteProduct.img
+                              }.png"
+                               alt="${favouriteProduct.title}" height="100">
+                              <div>
+                                  <p class="item__info--title">${
+                                    favouriteProduct.title
+                                  }</p>
+                              </div>
+                          </div>
+                      </td>
+                      <td>$${favouriteProduct.price}</td>
+                      <td>${
+                        favouriteProduct.sale
+                          ? "<span class='item__sale'>- " +
+                            favouriteProduct.salePercent +
+                            "%" +
+                            "</span>"
+                          : "-"
+                      }
+                      </td>
+                      <td>$${
+                        favouriteProduct.sale
+                          ? favouriteProduct.price -
+                            (favouriteProduct.price *
+                              favouriteProduct.salePercent) /
+                              100
+                          : favouriteProduct.price
+                      }
+                      </td>
+                      <td>
+                          <button class="item__favourite"><img src="images/product__favourite--true.png" alt="favourite" height="20"  data-product_id="${
+                            favouriteProduct.id
+                          }"></button>
+                      </td> </tr>`;
+
+    favouriteTable.appendChild(tr);
+  });
+
+  const favouriteButtons = document.querySelectorAll(".item__favourite");
+
+  favouriteButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      const productID = parseInt(e.target.dataset.product_id);
+
+      const index = userLogined.favourites.findIndex(
+        (value) => value === productID
+      );
+
+      userLogined.favourites.splice(index, 1);
+      localStorage.setItem("users", JSON.stringify(USERS));
+      document.querySelector(`#headerFavouritesCount`).innerText =
+        userLogined.favourites?.length ?? 0;
+
+      favouriteTable.innerHTML = "";
+      renderFavourite(user);
+    });
+  });
+}
+
+if (favouriteTable && userLogined) {
+  renderFavourite(userLogined);
+}
+
+// INDEX
+// =====
+
+const categoriesContainer = document.querySelector("#categoriesContainer");
+const AllProducts = {};
+
+if (categoriesContainer) {
+  PRODUCTS.forEach((product) => {
+    product.categories.forEach((category) => {
+      const productBox = document.createElement("div");
+      productBox.classList.add("product");
+      productBox.innerHTML = `<button class="product__favourite">
+                    <img src="images/product__favourite${
+                      userLogined && userLogined.favourites.includes(product.id)
+                        ? "--true"
+                        : ""
+                    }.png" 
+                        data-product_id="${product.id}"
+                        alt="favourite" 
+                    height="20">
+                    </button>
+    <img src="images/products/${product.img}.png" class="product__img" alt="${
+        product.title
+      }" height="80">
+    <p class="product__title">${product.title}</p>
+    ${
+      product.sale
+        ? `<div class="product__sale">
+                <span class="product__sale--old">$${product.price}</span>
+                <span class="product__sale--percent">- ${
+                  product.salePercent + "%"
+                }</span>
+                      </div>`
+        : ""
+    }
+    <div class="product__info">
+        <span class="product__price">$${
+          product.sale
+            ? product.price - (product.price * product.salePercent) / 100
+            : product.price
+        }</span>
+    </div>`;
+
+      if (!AllProducts[category]) {
+        const categorySection = document.createElement("section");
+        categorySection.classList.add("category");
+
+        categorySection.dataset.name = category;
+        categorySection.innerHTML = `<h2>${category}</h2>`;
+
+        const categoryWrapper = document.createElement("div");
+        categoryWrapper.classList.add("category__container");
+        AllProducts[category] = categoryWrapper;
+
+        categorySection.appendChild(categoryWrapper);
+        categoriesContainer.appendChild(categorySection);
+      }
+      AllProducts[category].appendChild(productBox);
+    });
+  });
+}
+
+const favouritesAll = document.querySelectorAll(".product__favourite");
+if (favouritesAll && !userLogined) {
+  favouritesAll.forEach((favourite) => {
+    favourite.addEventListener("click", (e) => {
+      e.preventDefault();
+      document.location.href = "login.html";
+    });
+  });
+}
+if (favouritesAll && userLogined) {
+  favouritesAll.forEach((favourite) => {
+    favourite.addEventListener("click", (e) => {
+      e.preventDefault();
+      const product_id = e.target.dataset.product_id;
+
+      if (product_id) {
+        if (userLogined.favourites.includes(parseInt(product_id))) {
+          const index = userLogined.favourites.findIndex(
+            (value) => value === parseInt(product_id) // строка в число
+          );
+
+          userLogined.favourites.splice(index, 1); //удалит 1єл по index
+          e.target.src = `images/product__favourite.png`;
+        } else {
+          userLogined.favourites.push(parseInt(product_id));
+          e.target.src = `images/product__favourite--true.png`;
+        }
+
+        document.querySelector(`#headerFavouritesCount`).innerText =
+          userLogined.favourites?.length ?? 0;
+
+        localStorage.setItem("users", JSON.stringify(USERS));
+      }
+    });
+  });
+}
